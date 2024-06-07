beffe707ca2ef477c9df721cd3c9b9d9790ab74e
AgataGomola <s27943@pjwstk.edu.pl>
2024-06-07 18:20:49 +0200

    added httppost

--- tutorial_9/tutorial_9/Controllers/TripsController.cs
+++ tutorial_9/tutorial_9/Controllers/TripsController.cs
@@ -67,6 +67,55 @@ public class TripsController : ControllerBase
         await _context.SaveChangesAsync();
         return NoContent();
     }
-    
+
+    [HttpPost("{id:int}/clients")]
+    public async Task<IActionResult> attachClient(int id, [FromBody] ClientTrip cl)
+    {
+        var trip = await _context.Trips.FindAsync(id);
+        if (trip == null)
+        {
+            return NotFound($"Trip with id {id} does not exist.");
+        }
+
+        if (trip.DateFrom < DateTime.Now)
+        {
+            return BadRequest("Trip already started.");
+        }
+        
+        var client = await _context.Clients.SingleOrDefaultAsync(c => c.Pesel == cl.IdClientNavigation.Pesel);
+        
+        if (client != null)
+        {
+            var isAssigned = await _context.ClientTrips.AnyAsync(t => t.IdClientNavigation.Pesel == cl.IdClientNavigation.Pesel && t.IdTrip == id);
+            if (isAssigned)
+            {
+                return BadRequest("Client is already assigned to this trip");
+            }
+            return BadRequest("Client already exists.");   
+        }
+        client = new Client()
+        {
+            FirstName = cl.IdClientNavigation.FirstName,
+            LastName = cl.IdClientNavigation.LastName,
+            Email = cl.IdClientNavigation.Email,
+            Telephone = cl.IdClientNavigation.Telephone,
+            Pesel = cl.IdClientNavigation.Pesel,
+        };
+        _context.Clients.Add(client);
+        await _context.SaveChangesAsync();
+
+        var clientTrip = new ClientTrip()
+        {
+           IdClient = client.IdClient,
+           IdTrip = id,
+           RegisteredAt = DateTime.Now,
+           PaymentDate = cl.PaymentDate,
+           IdClientNavigation = client,
+           IdTripNavigation = trip
+        };
+        _context.ClientTrips.Add(clientTrip);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
     
 }
